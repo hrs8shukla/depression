bd9d6fdc7af3f575c47a925bec35de342b8609f9
Harshit Shukla <harshit12shukla@gmail.com>
2022-02-01 23:58:08 +0530

    6

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,request, url_for, redirect, render_template
+from flask import Flask, request, url_for, redirect, render_template
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-model=pickle.load(open('depression.pkl','rb'))
+model = pickle.load(open('depression.pkl', 'rb'))
 
 
 @app.route('/')
@@ -12,18 +12,18 @@ def hello_world():
     return render_template("depression.html")
 
 
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
+    int_features = [int(x) for x in request.form.values()]
+    final = [np.array(int_features)]
     print(int_features)
     print(final)
-    prediction=model.predict_proba(final)
-
-    if prediction== 1:
-        return render_template('depression.html',pred='You might have depression {}'.format(prediction),bhai="kuch karna hain iska ab?")
+    prediction = model.predict_proba(final)
+    output = '{0:.{1}f}'.format(prediction[0][1], 2)
+    if output > str(0.5):
+        return render_template('depression.html', pred='\nProbability of depression is {} Please Consult with a Psychiatrist'.format(output), bhai="Unsafe")
     else:
-        return render_template('depression.html',pred='You dont have depression  {}'.format(prediction),bhai="Your Forest is Safe for now")
+        return render_template('depression.html', pred='You are safe.\n Probability of depression is {}'.format(output), bhai="Safe")
 
 
 if __name__ == '__main__':
